3fbafa972bd8f025d1d56fa9e63ee9388f630e6c
haeun kim <hagomy@naver.com>
2017-09-21 23:28:45 +0900

    Solve No.2741 from Baekjoon Online Judge

---
+++ src/algorithm/boj/Boj2741.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2017 haeun kim
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package algorithm.boj;
+
+import java.io.*;
+
+/**
+ * Created by haeun on 2017-09-20
+ * Site       : https://www.acmicpc.net/problem/2741
+ */
+public class Boj2741 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        for(int i=1; i<=n; i++){
+            System.out.println(i);
+        }
+    }
+
+}

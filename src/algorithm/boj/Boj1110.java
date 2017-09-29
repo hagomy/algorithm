7b03b0d246d26cc24ea18a30cb9b13a4b34e9720
haeun kim <hagomy@naver.com>
2017-09-29 21:38:48 +0900

    Solve No.1110 from Baekjoon Online Judge

---
+++ src/algorithm/boj/Boj1110.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2017 haeun kim
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package algorithm.boj;
+
+import java.io.*;
+
+/**
+ * Created by haeun on 2017-09-26
+ * Site       : https://www.acmicpc.net/problem/1110
+ */
+
+public class Boj1110 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int n1 = n;
+        int tmp;
+        int m = 0;
+        int count = 0;
+        while(true){
+            tmp = n/10 + n%10;
+            if(tmp < 10){
+                m = (n%10)*10 + tmp;
+            }else{
+                m = (n%10)*10 + tmp%10;
+            }
+
+            count++;
+            if(n1 == m){
+                break;
+            }
+
+            n = m;
+        }
+        System.out.println(count);
+    }
+
+}

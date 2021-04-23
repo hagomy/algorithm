3914aa99b5af4638501ad94b7f15fd9b93ed6e3b
hagomy <31856128+hagomy@users.noreply.github.com>
2021-04-23 14:40:42 +0900

    Update 섹션5.js

--- src/algorithm/Inflearn/섹션5.js
+++ src/algorithm/Inflearn/섹션5.js
@@ -39,20 +39,102 @@ let d=[3, 2, 5, 7];
 console.log(solution2(c,d));
 
 //
-function solution3(){
+function solution3(n,m,arr){
+    let answer=0;
+    for(let i=0;i<n;i++){
+        let sum=0;
+        for(let j=i;j<n;j++){
+            sum+=arr[j];
+            if(sum<m) continue;
+            else if(sum===m) answer++;
+            else break;
+        }
+    }
+    return answer;
+}
+console.log(solution3(8,6,[1,2,1,3,1,1,1,2]));
+
+//연속부분수열의 합이 특정숫자 M이하가 되는 경우가 몇 번
+function solution4(){
     
     return ;
 }
-console.log(solution3());
-
-
-
-
-
+console.log(solution4());
 
 //
-function solution(){
-    
-    return ;
+function solution5(n,k,arr){
+    let max=0;
+    for(let i=0;i<n;i++){
+        let sum=0;
+        for(let j=i;j<i+k;j++){
+            sum+=arr[j];
+        }
+        if(sum>max) max=sum;
+    }
+    return max;
 }
-console.log(solution());
+console.log(solution5(10,3,[12,15,11,20,25,10,20,19,13,15]));
+
+//학급 회장
+function solution6(s){
+    let answer;
+    let sH = new Map();
+    for(let x of s){
+        if(sH.has(x)) sH.set(x, sH.get(x)+1);
+        else sH.set(x, 1);
+    }
+    let max=Number.MIN_SAFE_INTEGER;
+    for(let [key, val] of sH){
+        if(val>max){
+            max=val;
+            answer=key;
+        }
+    }
+    return answer;
+}
+console.log(solution6("BACBACCACCBDEDE"));
+
+//
+function toMap(s){
+    let m = new Map();
+    for(let i of s){
+        if(m.has(i)) m.set(i, m.get(i)+1);
+        else m.set(i,1);
+    }
+    return m;
+}
+function solution7(s1,s2){
+    let m1 = toMap(s1);
+    let m2 = toMap(s2);
+
+    if(m1.size!==m2.size) return 'NO';
+    for(let [k,v] of m1){
+        if(m2.has(k) && m2.get(k)===v) continue;
+        else return 'NO';
+    }
+    return 'YES';
+}
+console.log(solution7('AbaAeCe','baeeACA'), solution7('abaCC','Caaab'));
+
+//
+function solution8(s,t){
+    let s1 ='';
+    let m2 = toMap(t);
+    let answer=0;
+
+    for(let i=0;i<s.length-t.length+1;i++){
+        s1 = s.slice(i,i+t.length);
+        let m1 = toMap(s1);
+        let d = true;
+        for(let [k,v] of m1){
+            if(m2.has(k) && m2.get(k)===v) continue;
+            else{
+                d=false;
+                break;
+            }
+        }
+        if(d) answer++;
+    }
+    return answer;
+}
+console.log(solution8('bacaAacba','abc'));
